c3ad9eee29ff978f6dbfa843fac8cfd2ac6bd99a
Ruby XIONG <rubyxiong@rubys-mbp.dhcp.mc.vanderbilt.edu>
2022-07-22 16:33:29 -0500

    Debug.

--- R/plotfun.R
+++ R/plotfun.R
@@ -25,9 +25,9 @@ plotGammaMix <- function(cfGMM.list, zero_include=FALSE, breaks=40){
   range.max <- rep(NA, num.comp)
 
   for(i in 1:num.comp){
-    new.comp <- dgamma(xs, shape=pars[1,i],rate=pars[2,i])
+    new.comp <- lambda[i] * dgamma(xs, shape=pars[1,i],rate=pars[2,i])
     range.max[i] <- max(new.comp)
-    ys[i, ] <- lambda[i] * dgamma(xs, shape=pars[1,i],rate=pars[2,i])
+    ys[i, ] <- lambda[i] * dgamma(xs, shape=pars[1,i],scale=pars[2,i])
   }
 
   # Generate Plot
